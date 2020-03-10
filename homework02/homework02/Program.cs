01cf640aeaf2e4e782baa388f1f4ef16bcef4f68
GoranJakovleski <zdravotoner@gmail.com>
2020-03-10 23:07:24 +0100

    Update Program.cs

--- homework02/homework02/Program.cs
+++ homework02/homework02/Program.cs
@@ -6,6 +6,34 @@ namespace homework02
     {
         static void Main(string[] args)
         {
+            //            Exercise 01
+            //Declare two string variables
+
+            //Initialize them and concatenate them in a new variable
+            //Declare two string variables
+            //Initialize them with the number 9 and 3
+            //Concatenate them in a new variable
+            //Print the results in the console
+            //string one = "Goran";
+            //string two = "Jakovleski";
+            //string three = "9";
+            //string ffor = "3";
+
+
+            //Console.WriteLine(one + two);
+            //Console.WriteLine(three + ffor);
+
+            //            Exercise 02
+            //Declare an integer and a string variable
+            //Initialize them and concatenate them in a new variable
+            //Print the result in the console
+
+            //string one = "goran";
+            //int two = 100;
+            //console.writeline(one + two);
+
+
+
             //Exercise 03
             //You have n credits on your mobile bill.
             //One SMS costs m credits.
@@ -28,28 +56,27 @@ namespace homework02
             // if its not converted, print in the console that user inserted invalid input
             //: if number / 2 has remainder 0 its even, else its odd
 
-            Console.WriteLine("Please enter some number");
-            var number = Console.ReadLine();
-            int converted;
-            bool isConverted = int.TryParse(number, out converted);
+            //Console.WriteLine("Please enter some number");
+            //int converted;
+            //bool isConverted = int.TryParse(Console.ReadLine(), out converted);
 
-            if (converted % 2 == 0)
-            {
+            //if (converted % 2 == 0)
+            //{
 
-                Console.WriteLine($"The number is add");
+            //    Console.WriteLine("The number is add");
 
-            }
+            //}
 
-            else if (converted > 0)
-            {
-                Console.WriteLine($"The number is even");
+            //else if (converted > 0)
+            //{
+            //    Console.WriteLine("The number is even");
 
-            }
-            else
-            {
+            //}
+            //else
+            //{
 
-                Console.WriteLine("You inserted invalid input");
-            }
+            //    Console.WriteLine("You inserted invalid input");
+            //}
 
 
             Console.ReadLine();
